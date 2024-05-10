1345cae0066f3f5efa637e1b001b84830ff753fb
Aron Kuna <akuna@ruc.dk>
2024-05-10 21:24:39 +0200

    Error page for nuxt content - when a document is not found

--- nuxt-template-project/pages/[...slug].vue
+++ nuxt-template-project/pages/[...slug].vue
@@ -1,5 +1,15 @@
 <template>
     <main class="prose max-w-screen-m mx-auto">
-      <ContentDoc />
+      <ContentDoc>
+        <template v-slot="{ doc }">
+          <article>
+            <h1>{{ doc.title }}</h1>
+            <ContentRenderer :value="doc" />
+          </article>
+        </template>
+        <template #not-found>
+          <h1>Document not found</h1>
+        </template>
+    </ContentDoc>
     </main>
   </template>
